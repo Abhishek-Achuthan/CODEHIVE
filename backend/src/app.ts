9139c99b6a5170fc9673f9f39396e8608e957136
Abhishek MA <abhishekachuthan693@gmail.com>
2025-10-27 19:35:49 +0530

    feat: Add dotenv and zod dependencies; refactor app structure for improved configuration

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,20 +1,40 @@
-import { Express } from "express";
-import express from "express";
+import 'reflect-metadata';
+import dotenv from 'dotenv';
+import express, { Express } from "express";
+dotenv.config();
+import { AuthRoute } from "./presentation/routes/AuthRoutes";
+import { MongodbConfig } from './config/MongodbConfig';
 
-export class Server {
+export class App {
   private readonly _app: Express;
 
   constructor() {
     this._app = express();
+    this.configMiddlewares();
+    this.configRoutes();
+    this.configDb();
+  }
+
+  configDb () {
+    MongodbConfig.connectDB();
+  }
+
+  configMiddlewares() {
+    this._app.use(express.urlencoded());
+    this._app.use(express.json());
+  }
+
+  configRoutes() {
+    const authRoute = new AuthRoute();
+    this._app.use('/api/auth',authRoute.getRoutes());
   }
 
   public listen() {
-    this._app.listen(3000, () => {
-      console.log(`server started at port ${3000}`);
+    this._app.listen(4000, () => {
+      console.log(`server started at port ${4000}`);
     });
   }
 }
 
-let app = new Server();
+const app = new App();
 app.listen();
-
