859dd090f6d8ff19fa835818e56a0e7f468a4a78
Abhishek MA <abhishekachuthan693@gmail.com>
2025-11-05 21:26:17 +0530

    fix(errorHandler): add ESLint directive to suppress warning for error handler function
    - Added an ESLint directive to the errorHandler function to prevent linting errors related to unused parameters.

--- backend/src/presentation/middlewares/errorHanlder.ts
+++ backend/src/presentation/middlewares/errorHanlder.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { BaseError } from "../../core/errors/BaseError";
 
+ // eslint-disable-next-line 
 export function errorHandler(err: Error,req:Request,res:Response,next:NextFunction) {
     if(err instanceof BaseError) {
         console.error("Custom Error:", err.message);
