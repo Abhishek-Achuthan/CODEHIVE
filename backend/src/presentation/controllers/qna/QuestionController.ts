c9d5ef838d516aa998f276f87e8439d66100ad72
Abhishek MA <abhishekachuthan693@gmail.com>
2025-11-19 15:52:22 +0530

    chore(qna): remove leftover console.log in list questions controller

--- backend/src/presentation/controllers/qna/QuestionController.ts
+++ backend/src/presentation/controllers/qna/QuestionController.ts
@@ -31,9 +31,9 @@ export class QuestionController {
   }
 
   async handleListQuestions(req: Request, res: Response, next: NextFunction) {
-    try {
+    try {                                             
       const data = QuestionListSchema.parse(req.query);
-      console.log(data)
+
       const questions = await this._listQuestionUseCase.execute(data);
 
       res.status(HttpStatus.OK).json({
